a8ec51fff5dca9abe37bf4899e5505dc07252de1
novemhariw <boot.fb.novem@gmail.com>
2023-07-08 22:59:13 +0700

    Add files via upload

---
+++ application/views/home/index.php
@@ -0,0 +1,276 @@
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand" href="#page-top">NOVEM HW</a>
+        <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portofolio</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- Masthead-->
+<header class="masthead bg-primary text-white text-center">
+    <div class="container d-flex align-items-center flex-column">
+        <!-- Masthead Avatar Image-->
+        <!-- <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="..." /> -->
+        <img class="masthead-avatar mb-2" src="<?= base_url('assets/'); ?>img/avataaars.svg" alt="logo" />
+        <!-- Masthead Heading-->
+        <h2 class="text-uppercase mb-0">Portofolio - NOVEM HW</h2>
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fa-solid fa-circle-check"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- Masthead Subheading-->
+        <p class="masthead-subheading font-weight-light mb-0"> Sistem Integrasi
+Kami Menyediakan Layanan  Implementasi Sistem Integrasi Teknologi Informasi Bagi Perusahaan Anda</p>
+    </div>
+</header>
+<!-- Portfolio Section-->
+<section class="page-section portfolio" id="portfolio">
+    <div class="container">
+        <!-- Portfolio Section Heading-->
+        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Portofolio</h2>
+        <!-- Icon Divider-->
+        <div class="divider-custom">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- Portfolio Grid Items-->
+        <div class="row justify-content-center">
+
+
+
+            <!-- Portfolio Website -->
+            <div class="col-md-6 col-lg-4 mb-4">
+                <a href="#" target="blank">
+                    <div class="portfolio-item mx-auto">
+                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                            <div class="portfolio-item-caption-content text-center text-white"><i class="fa-solid fa-circle-check fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="<?= base_url('assets/'); ?>img/portfolio/implementasi.png" alt="..." />
+                </a>
+				<p class="masthead-subheading font-weight-light mb-0"> <h3>Jaringan Komputer</h3>
+Kami melayani jasa solusi dan implementasi pembangunan jaringan komputer baik LAN maupun WAN.</p>
+            </div>
+        </div>
+
+
+
+        <!-- Portfolio  Tata Persuratan online -->
+        <div class="col-md-6 col-lg-4 mb-4">
+            <a href="#" target="blank">
+                <div class="portfolio-item mx-auto">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white"><i class="fa-solid fa-circle-check fa-3x"></i></div>
+                    </div>
+                    <img class="img-fluid" src="<?= base_url('assets/'); ?>img/portfolio/icon-kabel-data.png" alt="..." />
+            </a>
+			<p class="masthead-subheading font-weight-light mb-0"> <h3>Instalasi Cabling</h3>
+Layanan desain dan instalasi kabel data : kabel UTP dan kabel FO untuk mendukung infrastruktur jaringan komputer.</p>
+        </div>
+    </div>
+
+
+    <!-- Portfolio Absen Digital-->
+    <div class="col-md-6 col-lg-4 mb-4">
+        <a href="#" target="blank">
+            <div class="portfolio-item mx-auto">
+                <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                    <div class="portfolio-item-caption-content text-center text-white"><i class="fa-solid fa-circle-check fa-3x"></i></div>
+                </div>
+                <img class="img-fluid" src="<?= base_url('assets/'); ?>img/portfolio/wifi-ikon3.png" alt="..." />
+        </a>
+		<p class="masthead-subheading font-weight-light mb-0"> <h3>Solusi Wireless</h3>
+Layanan desain jaringan berbasis wireless dengan perangkat berkinerja tinggi baik dari sisi kapasitas maupun keamanan.</p>
+    </div>
+    </div>
+
+
+    <!-- Portfolio Buku Tamu-->
+    <div class="col-md-6 col-lg-4 mb-0 mb-lg-0">
+        <a href="#" target="blank">
+            <div class="portfolio-item mx-auto">
+                <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                    <div class="portfolio-item-caption-content text-center text-white"><i class="fa-solid fa-circle-check fa-3x"></i></div>
+                </div>
+                <img class="img-fluid" src="<?= base_url('assets/'); ?>img/portfolio/ikon-vikon-150x150.png" alt="..." />
+        </a>
+		<p class="masthead-subheading font-weight-light mb-0"> <h3>Telekomunikasi</h3>
+Layanan desain dan implementasi komunikasi berbasis IP Convergence: video conference dan IP telephony.</p>
+    </div>
+    </div>
+
+    <!-- Portfolio Buku Tamu-->
+    <div class="col-md-6 col-lg-4 mb-0 mb-lg-0">
+        <a href="#" target="blank">
+            <div class="portfolio-item mx-auto">
+                <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                    <div class="portfolio-item-caption-content text-center text-white"><i class="fa-solid fa-circle-check fa-3x"></i></div>
+                </div>
+                <img class="img-fluid" src="<?= base_url('assets/'); ?>img/portfolio/ikon-telepon-150x150.png" alt="..." />
+        </a>
+		<p class="masthead-subheading font-weight-light mb-0"> <h3>Otomatisasi</h3>
+Layanan desain dan implementasi automation berbasis teknologi Internet of Things untuk mendukung digitalisasi industri.</p>
+    </div>
+    </div>
+
+    <!-- Portfolio Buku Tamu-->
+    <div class="col-md-6 col-lg-4 mb-0 mb-lg-0">
+        <a href="#" target="blank">
+            <div class="portfolio-item mx-auto">
+                <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                    <div class="portfolio-item-caption-content text-center text-white"><i class="fa-solid fa-circle-check fa-3x"></i></div>
+                </div>
+                <img class="img-fluid" src="<?= base_url('assets/'); ?>img/portfolio/ikon-rental-komputer.png" alt="..." />
+        </a>
+		<p class="masthead-subheading font-weight-light mb-0"> <h3>Rental Perangkat</h3>
+Layanan rental perangkat jaringan ataupun perangkat komputer / multimedia untuk acara sesaat atau kantor sementara.</p>
+		
+    </div>
+    </div>
+
+
+
+    </div>
+    </div>
+</section>
+<!-- About Section-->
+<section class="page-section bg-primary text-white mb-0" id="about">
+    <div class="container">
+        <!-- About Section Heading-->
+        <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-address-card"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- About Section Content-->
+        <div class="row">
+            <div class="col-lg-4 ms-auto">
+                <p class="lead"><b>Sistem Integrasi</b>
+Perusahaan bidang teknologi informasi dengan fokus pada integrasi sistem infrastruktur jaringan, server, PC, dan software biasa disebut sebagai perusahaan sistem integrator yang bertanggungjawab agar seluruh perangkat yang berbeda-beda tsb dapat bekerja sebagai suatu kesatuan sistem TI. </p>
+            </div>
+            <div class="col-lg-4 me-auto">
+                <p class="lead"><b>Pengalaman Kami</b>
+Kami telah melayani jasa sistem integrasi sejak tahun 2013 di berbagai jenis proyek, antara lain: instalasi jaringan kabel FO dan data, instalasi perangkat wireless, video conference, IP telephony, dsb. Beberapa klien kami antara lain : Indosat, Simpatindo Multi Media, Aquarius Boutique Sampit, Inul Vizta, Studio Family Karaoke Sampit. </p>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- Contact Section-->
+<section class="page-section" id="">
+       <div class=malasngoding-slider>
+		<div class=isi-slider>
+			<img src="<?= base_url('assets/'); ?>img/gambar/gambar1.jpg" alt="Gambar 1">
+			<img src="<?= base_url('assets/'); ?>img/gambar/gambar2.jpeg" alt="Gambar 2">
+			<img src="<?= base_url('assets/'); ?>img/gambar/gambar3.jpg" alt="Gambar 3">
+			<img src="<?= base_url('assets/'); ?>img/gambar/gambar9.jpg" alt="Gambar 5">
+		</div>
+	</div>
+   
+</section>
+<!-- Contact Section-->
+<section class="page-section" id="contact">
+    <div class="container">
+        <!-- Contact Section Heading-->
+        
+        <!-- Icon Divider-->
+        <div class="divider-custom">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-address-book"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+		 
+        <!-- About Section Content-->
+        <div class="row justify-content-center">
+
+            <div class="col-lg-5">
+			
+                <p class="lead">Bagi Anda yang ingin mengetahui informasi lebih lanjut mengenai layanan kami dan secara khusus meminta penawaran, presentasi. Silahkan mengontak Tim Sales kami melalui formulir isian yang telah disediakan untuk mengajukan permohonan informasi yang dimaksud dengan cara mengklik tombol “Chat Whatsapp” di bagian bawah ini. Peringatan: Kami hanya melayani permintaan informasi lebih lanjut melalui request yang dikirimkan dengan formulir isian yang telah disediakan. Terimakasih atas perhatian dan pengertiannya.</p>
+            <a href="https://wasap.at/usxxP8" title="klik to Chat">
+   <img src="<?= base_url('assets/'); ?>img/whatsapp.png" alt="wa logo" width="400" border=0>
+</a>
+			</div>
+        </div>
+
+        <!-- Contact Section Form-->
+        <div class="row justify-content-center">
+            <div class="col-lg-8 col-xl-7">
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Footer-->
+<footer class="footer text-center">
+    <div class="container">
+        <div class="row">
+            <!-- Footer Location-->
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">Location</h4>
+                <p class="lead mb-0">
+                    Jl Barito. Kota Sampit.
+                    <br />
+                    Provinsi Kalimantan Tengah, Indonesia.
+                </p>
+            </div>
+            <!-- Footer Social Icons-->
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">Social Media</h4>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/novem.hw/"><i class="fab fa-fw fa-facebook-f"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/novemhw"><i class="fab fa-fw fa-twitter"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://instagram.com/novemhw"><i class="fa-brands fa-instagram"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="#"><i class="fa-brands fa-youtube"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://github.com/novemhariw"><i class="fab fa-github"></i></a>
+
+            </div>
+            <!-- Footer About Text-->
+            <div class="col-lg-4">
+                <h4 class="text-uppercase mb-4">About</h4>
+                <p class="lead mb-0">
+                    Website ini bertujuan untuk menampilkan hasil projek yang pernah dikerjakan.
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+<script>
+var isNS = (navigator.appName == "Netscape") ? 1 : 0;
+if(navigator.appName == "Netscape") document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
+function mischandler(){
+return false;
+}
+function mousehandler(e){
+var myevent = (isNS) ? e : event;
+var eventbutton = (isNS) ? myevent.which : myevent.button;
+if((eventbutton==2)||(eventbutton==3)) return false;
+}
+document.oncontextmenu = mischandler;
+document.onmousedown = mousehandler;
+document.onmouseup = mousehandler;
+</script>
+
+<script>
+    /*function check(e)
+    {
+    alert(e.keyCode);
+    }*/
+    document.onkeydown = function(e) {
+            if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
+                alert('not allowed');
+            }
+            return false;
+    };
+    </script>
---
+++ application/views/templates/footer.php
@@ -0,0 +1,13 @@
+<!-- Copyright Section-->
+<div class="copyright py-4 text-center text-white">
+    <div class="container"><small>Copyright &copy; <a href="#" target="_blank" rel="noopener noreferrer"> Novem HW</a></small></div>
+</div>
+
+<!-- Bootstrap core JS-->
+<script src="<?= base_url('assets/'); ?>js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="<?= base_url('assets/'); ?>js/scripts.js"></script>
+
+</body>
+
+</html>
